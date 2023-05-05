3071e597470a7045b127d84a74030ae4a6529640
José Weslles da Cruz Souza <joseweslles_b2@hotmail.com>
2023-05-04 23:29:38 -0300

    Criando Projeto de TCC

---
+++ backend/src/main/java/com/sisavcpa/sisavcpa/SisavcpaApplication.java
@@ -0,0 +1,13 @@
+package com.sisavcpa.sisavcpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisavcpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisavcpaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sisavcpa/sisavcpa/SisavcpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisavcpa.sisavcpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisavcpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
